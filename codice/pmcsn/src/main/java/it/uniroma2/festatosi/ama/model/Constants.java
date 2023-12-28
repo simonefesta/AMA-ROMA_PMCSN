fbc1e304c8735ab79887bcc194c30753ecfb8eaf
MicheleTosi <micheletosi98@gmail.com>
2023-12-28 21:33:23 +0100

    aggiunto maven

---
+++ codice/pmcsn/src/main/java/it/uniroma2/festatosi/ama/model/Constants.java
@@ -0,0 +1,79 @@
+package it.uniroma2.festatosi.ama.model;
+
+// Qui introduciamo le probabilità e costanti varie
+public class Constants {
+
+    public static double START   = 0.0;            /* inizio lavoro giornaliero       */
+    public static double STOP    = 86400;        /*  terminazione lavoro giornaliero (24 ore in secondi) */
+    public static int    SERVERS_SCARICO = 5;
+    public static int    SERVERS_ACCETTAZIONE = 4;
+    public static int    SERVERS_GOMMISTA = 2;
+    public static int    SERVERS_CARROZZERIA = 3;
+    public static int    SERVERS_ELETTRAUTI = 1;
+    public static int    SERVERS_CARPENTERIA = 6;
+    public static int    SERVERS_MECCANICA = 5;
+    public static int    SERVERS_CHECKOUT = 1;
+
+
+
+  // Entrata diretta per lo scarico dei rifiuti
+
+    public static final double P1 = 0.64;
+
+    // probabilità guasto da identificare
+    public static final double Q1 = 0.36; //1-P1
+
+    /*
+    Zona AUTOFFICINA
+     */
+
+    // probabilità mezzo non riparabile
+    public static final double Q2 = 0.02;
+
+
+    // probabilità difetto tipologia "gommista"
+    public static final double P2 = 0.3;
+
+    // probabilità difetto tipologia "carrozzeria"
+    public static final double P3 = 0.08;
+
+    // ---- SKIPP PROB ----
+    // probabilità difetto tipologia "elettrauti"
+    public static final double P4 = 0.1;
+
+    // probabilità difetto tipologia "carpenteria meccanica"
+    public static final double P5 = 0.2;
+
+    // probabilità difetto tipologia "meccanica"
+    public static final double P6 = 0.3;
+
+    public static final double [] Percentuali_OFFICINA = {Q2,  P2,  P3,  P4,  P5,  P6};
+
+
+    /*
+       Zona CHECKOUT
+     */
+
+    // probabilità di uscire dal sistema senza checkout
+    public static final double P7 = 0.7;
+
+    // probabilità di uscire dal sistema previo checkout
+    public static final double Q7 = 0.3;
+
+
+    // ---- ARRIVAL RATES [req/sec]----
+
+    public static final double LAMBDA = 0.3; // ad ora è a caso, bisogna ragionarci su!
+
+
+    // ---- SERVICE RATES  [sec] ----
+    public static final double accettazione_SR = 10*60;
+    public static final double scarico_SR = 15*60;
+    public static final double riparazione_SR = 2*3600; //per tutte le officine (2 ore?)
+
+    public static final double checkout_SR = 20*60;
+
+
+
+
+}
---
+++ codice/pmcsn/src/main/java/it/uniroma2/festatosi/ama/model/Events.java
@@ -0,0 +1,78 @@
+package it.uniroma2.festatosi.ama.model;
+
+//Qui definiamo gli eventi caratterizzanti il sistema
+public class Events {
+
+    /* -- Sistema completo */
+    public static final int EVENT_ARRIVE_SISTEMA = 1;
+    public static final int EVENT_DEPARTURE_SISTEMA = 1;
+
+    public static final int ALL_EVENTS_SYSTEM = EVENT_ARRIVE_SISTEMA + EVENT_DEPARTURE_SISTEMA;
+
+    /* -- Accettazione */
+    public static final int EVENT_ARRIVE_ACCETTAZIONE = 1;
+    public static final int EVENT_DEPARTURE_ACCETTAZIONE = 1;
+    public static final int EVENT_BROKEN_ACCETTAZIONE = 1; //mezzo non riparabile.
+
+    public static final int ALL_EVENTS_ACCETTAZIONE = EVENT_ARRIVE_ACCETTAZIONE + EVENT_DEPARTURE_ACCETTAZIONE + EVENT_BROKEN_ACCETTAZIONE;
+
+
+    /* -- Autofficina */
+
+    public static final int EVENT_ARRIVE_GOMMISTA = 1;
+    public static final int EVENT_DEPARTURE_GOMMISTA = 1;
+
+    public static final int ALL_EVENTS_GOMMISTA = EVENT_ARRIVE_GOMMISTA + EVENT_DEPARTURE_GOMMISTA;
+
+    public static final int EVENT_ARRIVE_CARROZZERIA = 1;
+    public static final int EVENT_DEPARTURE_CARROZZERIA = 1;
+
+    public static final int ALL_EVENTS_CARROZZERIA = EVENT_ARRIVE_CARROZZERIA + EVENT_DEPARTURE_CARROZZERIA;
+
+
+    public static final int EVENT_ARRIVE_ELETTRAUTI = 1;
+    public static final int EVENT_DEPARTURE_ELETTRAUTI = 1;
+
+    public static final int ALL_EVENTS_ELETTRAUTI = EVENT_ARRIVE_ELETTRAUTI + EVENT_DEPARTURE_ELETTRAUTI;
+
+
+    public static final int EVENT_ARRIVE_CARPENTERIA = 1;
+    public static final int EVENT_DEPARTURE_CARPENTERIA = 1;
+
+    public static final int ALL_EVENTS_CARPENTERIA = EVENT_ARRIVE_CARPENTERIA + EVENT_DEPARTURE_CARPENTERIA;
+
+
+    public static final int EVENT_ARRIVE_MECCANICA = 1;
+    public static final int EVENT_DEPARTURE_MECCANICA = 1;
+
+    public static final int ALL_EVENTS_MECCANICA = EVENT_ARRIVE_MECCANICA + EVENT_DEPARTURE_MECCANICA;
+
+
+
+    /* -- Scarico rifiuti */
+    public static final int EVENT_ARRIVE_SCARICO = 1;
+    public static final int EVENT_DEPARTURE_SCARICO = 1;
+
+    public static final int ALL_EVENTS_SCARICO = EVENT_ARRIVE_SCARICO + EVENT_DEPARTURE_SCARICO;
+
+
+    /* -- CheckOut */
+    public static final int EVENT_ARRIVE_CHECKOUT = 1;
+    public static final int EVENT_DEPARTURE_CHECKOUT = 1;
+
+    public static final int ALL_EVENTS_CHECKOUT = EVENT_ARRIVE_CHECKOUT + EVENT_DEPARTURE_CHECKOUT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
