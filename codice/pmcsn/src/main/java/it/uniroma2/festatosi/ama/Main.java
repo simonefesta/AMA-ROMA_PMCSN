fc9c1d2db430ce44a2b92774c564d7712a242788
MicheleTosi <micheletosi98@gmail.com>
2024-01-05 11:58:14 +0100

    rimossi controller non più necessari

--- codice/pmcsn/src/main/java/it/uniroma2/festatosi/ama/Main.java
+++ codice/pmcsn/src/main/java/it/uniroma2/festatosi/ama/Main.java
@@ -1,8 +1,5 @@
 package it.uniroma2.festatosi.ama;
 
-import it.uniroma2.festatosi.ama.controller.ControllerAccettazione;
-import it.uniroma2.festatosi.ama.controller.ControllerGommista;
-import it.uniroma2.festatosi.ama.controller.ControllerScarico;
 import it.uniroma2.festatosi.ama.controller.ControllerSistema;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
